f11201c65c55396799caf3d151dc07b30035a333
murizzsu <93246525+murizzsu@users.noreply.github.com>
2021-12-07 09:59:44 +0700

    Create final.php

---
+++ final.php
@@ -0,0 +1,31 @@
+<?php 
+//Menghitung Volume Balok
+
+class Balok{
+    private $p,$l,$t;
+
+    public function __construct($panjang,$lebar,$tinggi){
+        $this->p = $panjang;
+        $this->l = $lebar;
+        $this->t = $tinggi;
+    }
+
+    final public function rumus(){
+        $str = "Rumus Volume Balok Yaitu Panjang x Lebar x Tinggi <br>";
+        return $str;
+    }
+
+    final public function hitung(){
+        $info = "Panjang = {$this->p} cm^3, Lebar = {$this->l} cm^3, Tinggi = {$this->t} cm^3 <br>";
+        $hasil1 = "Hasil : ";
+        $hasil = $this->p * $this->l * $this->t;
+        return $info.$hasil1.$hasil;
+    }
+
+}
+
+$volume = new Balok(10,3,5);
+echo $volume->rumus();
+echo $volume->hitung();
+
+?>
